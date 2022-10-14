a0843176fd3f001430da981bc316e2b10d93860a
oluwakunmi <ubeydatoluwakunmi@gmail.com>
2022-10-13 21:05:51 -0700

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 /**
- *  * main - entry point
- *   *
- *    * Return: Always 0
- *     */
+   * main - entry point
+    *
+     * Return: Always 0
+      */
 
 int main(void)
 
 {
+
 	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
+
 }
