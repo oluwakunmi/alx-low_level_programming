156fa8cc0261fc2c8ab10a9f3149ab306d7c1d58
oluwakunmi <ubeydatoluwakunmi@gmail.com>
2023-05-19 05:01:44 -0700

    adding puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point with no argument
+ *
+ * Puts - prints character on the screen
+ *
+ * Return: 0 if successfull else 1 on error
+ */
+int main(void)
+{
+	char str[] = "Programming is like building a multilingual puzzle!\n";
+
+	puts(str);
+
+	return (0);
+}
