3e9428c137ccc77cc03d2ba56c6c826b4d047e4e
oluwakunmi <ubeydatoluwakunmi@gmail.com>
2023-05-19 05:26:30 -0700

    add printf file

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * printf - print character on the screen
+ *
+ * Return: Always 0 if successfull
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+
+}
