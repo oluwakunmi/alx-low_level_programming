9256ecc6a7ce7af4165396aa44a51249bb1867c3
oluwakunmi <ubeydatoluwakunmi@gmail.com>
2023-05-20 04:34:58 -0700

    adding size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,23 +6,23 @@
  */
 int main(void)
 {
-	char 'a';
+	char a;
 	int b;
 	long int c;
 	long long int d;
 	float e;
 
-	size a = sizeof(char);
-	size b = sizeof(int);
-	size c = sizeof(long int);
-	size d = sizeof(long long int);
-	size e = sizeof(float);
+	size_t size_a = sizeof(a);
+	size_t size_b = sizeof(b);
+	size_t size_c = sizeof(c);
+	size_t size_d = sizeof(d);
+	size_t size_e = sizeof(e);
 
-	printf("Size of a char:%c byte(s) /n", a);
-	printf("Size of an int:%d byte(s) /n", b);
-	printf("Size of a long int:%i byte(s) /n", c);
-	printf("Size of a long long int:%i byte(s) /n", d);
-	printf("Size of a float:%f byte(s)  /n", e);
+	printf("Size of a char:%zu byte(s)\n", size_a);
+	printf("Size of an int:%zu byte(s)\n", size_b);
+	printf("Size of a long int:%zu byte(s)\n", size_c);
+	printf("Size of a long long int:%zu byte(s)\n", size_d);
+	printf("Size of a float:%zu byte(s)\n", size_e);
 
 	return (0);
 }
