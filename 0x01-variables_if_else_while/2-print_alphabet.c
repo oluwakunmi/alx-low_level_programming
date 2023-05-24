c4514044dfa2886e8f6d8f3b1c7b3afe07823036
oluwakunmi <ubeydatoluwakunmi@gmail.com>
2023-05-24 15:08:58 -0700

    alphatet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: Always zero if successfull
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+		putchar(\n);
+
+	}
+
+	return (0);
+
+
+}
