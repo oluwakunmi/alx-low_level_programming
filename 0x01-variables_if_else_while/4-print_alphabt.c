93e0b274d9ef02181ecd942818a0c1da9ed7e01e
oluwakunmi <ubeydatoluwakunmi@gmail.com>
2023-05-27 12:59:46 -0700

    print alphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char lower;
+
+	for (lower = 'a'; lower <= 'z'; lower++)
+	{
+		if (lower != 'e' && lower != 'q')
+			continue;
+				putchar(lower);
+	}
+
+	putchar('\n');
+	return (0);
+}
