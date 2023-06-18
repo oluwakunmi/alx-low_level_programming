f947c43af94a6cccdec2fc3a15bc89fde628fca5
oluwakunmi <ubeydatoluwakunmi@gmail.com>
2023-06-18 12:11:06 -0700

    debug

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,73 +1,20 @@
 #include <stdio.h>
-#include "main.h"
+#include <stdlib.h>
 
 /**
- * _atoi - converts a string to an integer
- * @s: string to be converted
- *
- * Return: the int converted from the string
- */
-int _atoi(char *s)
-{
-	int a, b, c, len, d, digit;
-
-	a = 0;
-	b = 0;
-	c = 0;
-	len = 0;
-	d = 0;
-	digit = 0;
-
-	while (s[len] != '\0')
-		len++;
-
-	while (a < len && d == 0)
-	{
-		if (s[a] == '-')
-			++b;
-
-		if (s[a] >= '0' && s[a] <= '9')
-		{
-			digit = s[a] - '0';
-			if (b % 2)
-				digit = -digit;
-			c = c * 10 + digit;
-			d = 1;
-			if (s[a + 1] < '0' || s[a + 1] > '9')
-				break;
-			d = 0;
-		}
-		a++;
-	}
-
-	if (d == 0)
-		return (0);
-
-	return (n);
-}
-
-/**
- * main - multiplies two numbers
- * @argc: number of arguments
- * @argv: array of arguments
- *
- * Return: 0 (Success), 1 (Error)
+ * main - multiplies two numbers.
+ * @argc: number of command line arguments.
+ * @argv: array that contains the program command line arguments.
+ * Return: 0 - success.
  */
 int main(int argc, char *argv[])
 {
-	int result, num1, num2;
-
-	if (argc < 3 || argc > 3)
+	if (argc != 3)
 	{
+
 		printf("Error\n");
 		return (1);
 	}
-
-	num1 = _atoi(argv[1]);
-	num2 = _atoi(argv[2]);
-	result = num1 * num2;
-
-	printf("%d\n", result);
-
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 	return (0);
 }
--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,85 +1,28 @@
+#include <stdlib.h>
+#include <ctype.h>
 #include <stdio.h>
-#include "main.h"
-
 /**
- * _atoi - converts a string to an integer
- * @s: string to be converted
- *
- * Return: the int converted from the string
- */
-int _atoi(char *s)
-{
-	int a, b, c, len, d, digit;
-
-	a = 0;
-	b = 0;
-	c = 0;
-	len = 0;
-	d = 0;
-	digit = 0;
-
-	while (s[len] != '\0')
-		len++;
-
-	while (a < len && d == 0)
-	{
-		if (s[a] == '-')
-			++b;
-
-		if (s[a] >= '0' && s[a] <= '9')
-		{
-			digit = s[a] - '0';
-			if (b % 2)
-				digit = -digit;
-			c = c * 10 + digit;
-			d = 1;
-			if (s[a + 1] < '0' || s[a + 1] > '9')
-				break;
-			d = 0;
-		}
-		a++;
-	}
-
-	if (d == 0)
-		return (0);
-
-	return (n);
-}
-
-/**
- * main - adds two positive number
- * @argc: number of arguments
- * @argv: array of arguents
- *
- * Return: 0 (Success), or 1 (Success)
+ * main - adds positive numbers.
+ * @argc: number of command line arguments.
+ * @argv: array that contains the program command line arguments.
+ * Return: 0 - success.
  */
 int main(int argc, char *argv[])
 {
-	int sum, num, a, f, k;
+	int i, j, add = 0;
 
-	sum = 0;
-
-	for (a = 1; a < argc; a++)
+	for (i = 1; i < argc; i++)
 	{
-		for (f = 0; argv[a][f] != '\0'; f++)
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			if (argv[a][f] > '9' || argv[a][f] < '0')
+			if (!isdigit(argv[i][j]))
 			{
-				puts("Error");
+				printf("Error\n");
 				return (1);
 			}
 		}
+		add += atoi(argv[i]);
 	}
-
-	for (k = 1; k < argc; k++)
-	{
-		num = _atoi(argv[k]);
-		if (num >= 0)
-		{
-			sum += num;
-		}
-	}
-
-	printf("%d\n", sum);
+	printf("%d\n", add);
 	return (0);
 }
