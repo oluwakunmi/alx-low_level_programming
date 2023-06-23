d10da6e382a93eae2740660886c1b140bc57b5e7
oluwakunmi <ubeydatoluwakunmi@gmail.com>
2023-06-23 12:29:35 -0700

    preprocessor

---
+++ 0x0D-preprocessor/1-main.c
@@ -0,0 +1,19 @@
+#include "1-pi.h"
+#include "1-pi.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    float a;
+    float b;
+
+    b = 98;
+    a = PI * b * b;
+    printf("%.3f\n", a);
+    return (0);
+}
