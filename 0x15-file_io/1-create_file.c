a3ba459ebdfaf9a19c1fbb2adf18bac0b370c4d4
oluwakunmi <ubeydatoluwakunmi@gmail.com>
2023-10-06 07:33:06 -0700

    update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 			len++;
 	}
 
-	x = open(filename, X_CREAT | X_RDWR | X_TRUNC, 0600);
+	x = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	w = write(x, text_content, len);
 
 	if (x == -1 || w == -1)
